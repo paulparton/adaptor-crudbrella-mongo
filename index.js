db1ab0d076de12f225f6e2831194093e152b7694
paulparton <paulparton@gmail.com>
2014-12-07 09:35:54 +0000

    complete rewrite to match new adaptor loader

--- index.js
+++ index.js
@@ -1,36 +1,9 @@
 var _  = require('lodash'),
-		ObjectId = require('mongodb').ObjectID;
+		ObjectId = require('mongodb').ObjectID,
+		validId;
 
-module.exports =  {
-
-	//Create a new record
-	create: function (options){
-
-		return function(req, res){
-
-			//If no options are provided default to an empty object
-			options = options || {};
-
-			//Save the object into the collection
-			this.collection.insert(req.body, function(err, result) {
-
-				//Pass results and req/res to the dbCallback
-				this.dbCallback({
-					req: req, 
-					res: res, 
-					err: err, 
-					result: result,
-					onSuccess:options.onSuccess || this.successCallback,
-					onError:options.onError || this.errorCallback
-				});
-
-			}.bind(this));
-
-		}.bind(this);
-
-	},
 	//Check if a given string is a valid mongo ObjectId
-	validId: function(id){
+	validId = function(id){
 
 		checkForHexRegExp = /^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i;
 
@@ -40,127 +13,88 @@ module.exports =  {
 			return false;
 		}
 
+	};
+
+
+module.exports =  {
+
+	//Create a new record
+	create: function (options, callback){
+
+		//Save the object into the collection
+		this.collection.insert(options.body, function(err, result) {
+
+			if(result.length === 1){
+				result = result[0];
+			}
+
+			//Pass results and req/res to the callback
+			return callback(err, result);
+
+		}.bind(this));
+
 	},
+
 	//Read an existing record / records
-	read: function(options){
+	read: function(options, callback){
 
-		return function (req, res){
+		_.each(options.query, function(item, key){
+			if(validId(item)){
+				options.query[key] = ObjectId(item);
+			}
+		}.bind(this));
 
-			//If no options are provided default to an empty object
-			options = options || {};
+		//Find using optional query or find all
+		this.collection.find(options.query || {}).toArray(function(err, result){
 
-			//If no query has been provided default to an empty object
-			options.query = options.query || {};
+			if(result.length === 1){
+				result = result[0];
+			}
 
-			//Create a local copy of the query definition to be parsed
-			var query = JSON.parse(JSON.stringify(options.query)) || {};
+			callback(err, result);
 
-			//Loop through every object in query
-			_.each(query, function(item, key){
-
-				//If the value starts with a ^ use the item value as a key for req.params
-				if(item[0] === "^"){
-
-					if(this.validId(req.params[item.replace("^","")])){	
-
-						query[key] = ObjectId(req.params[item.replace("^","")]);
-
-					}else{
-
-						query[key] = req.params[item.replace("^","")];	
-
-					}
-					
-				}
-
-			}.bind(this));
-
-			//Find using optional query or find all
-			this.collection.find(query || {}).toArray(function(err, items){
-
-				if(items.length === 1){
-					items = items[0];
-				}
-
-				//Pass results and req/res to the dbCallback
-				this.dbCallback({
-					req: req, 
-					res: res, 
-					err: err, 
-					result: items,
-					onSuccess:options.onSuccess || this.successCallback,
-					onError:options.onError || this.errorCallback
-				});
-
-			}.bind(this));
-
-		}.bind(this);
+		}.bind(this));
 
 	},
 
 	//Update a record
-	update: function(options){
+	update: function(options, callback){
+		
+		_.each(options.query, function(item, key){
+			if(validId(item)){
+				options.query[key] = ObjectId(item);
+			}
+		}.bind(this));
 
-		return function(req, res){
+		//If the id has been included in the body, remove it
+		if(options.body._id){
+			delete options.body._id;
+		}
 
-			var requestId;
-
-			//If no options are provided default to an empty object
-			options = options || {};
-
-			//If the id has been included in the body, remove it
-			requestId = ObjectId(req.params.id); 
-			delete req.body._id;
+		//Check if the body contains any populated fields and depopulate them
+		this.collection.update(options.query,{$set: options.body}, function(err, result){
 			
-			//Check if the body contains any populated fields and depopulate them
-			this.collection.update({'_id': requestId},{$set: req.body}, function(err, result){
+			callback(err, result);
 				
-				if(!err){
-					result = req.body;
-				}
-				
-				this.dbCallback({
-					req: req, 
-					res: res, 
-					err: err, 
-					result: result,
-					onSuccess:options.onSuccess || this.successCallback,
-					onError:options.onError || this.errorCallback
-				});
-				
-			}.bind(this));
-
-		}.bind(this);
+		}.bind(this));
 
 	},
 
 	//Delete a record
-	delete: function(options){
+	delete: function(options, callback){
 
-		return function(req, res){
+		_.each(options.query, function(item, key){
+			if(validId(item)){
+				options.query[key] = ObjectId(item);
+			}
+		}.bind(this));
+		
+		//Use dryCrud.read to find the document to be deleted
+		this.collection.remove(options.query, function(err, result){
 
-			//If no options are provided default to an empty object
-			options = options || {};
+			return callback(err, result)
 
-			//If the id has been included in the body, remove it
-			if(req.body._id) { delete req.body._id; }
-
-			//Use dryCrud.read to find the document to be deleted
-			this.collection.remove({_id: ObjectId(req.params.id)}, function(err, result){
-
-				//Pass results and req/res to the dbCallback
-				this.dbCallback({
-					req: req, 
-					res: res, 
-					err: err, 
-					result: "",
-					onSuccess:options.onSuccess || this.successCallback,
-					onError:options.onError || this.errorCallback
-				});
-
-			}.bind(this));
-
-		}.bind(this);
+		}.bind(this));
 
 	}
 
